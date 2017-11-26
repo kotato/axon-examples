21f5164363b2449f473f0b396a0627854bce5d35
Ramon Aranda <ramon.aranda.r@gmail.com>
2017-11-26 11:39:45 +0100

    Add controller Refactor application file folder Add configuration files (still empty)

--- ecomerce-cart/src/main/kotlin/EcommerceApplication.kt
+++ ecomerce-cart/src/main/kotlin/EcommerceApplication.kt
@@ -1,5 +1,3 @@
-package context.ecommerce
-
 import cqrs.application.EnableCqrs
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
