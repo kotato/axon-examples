3bfd7dfae77a99a05748df8002ab5adc9d2a4db5
Ramon Aranda <ramon.aranda.r@gmail.com>
2018-01-20 12:12:08 +0100

    Remove old delta matcher Fix test

--- ecomerce-cart/src/test/kotlin/com/kotato/context/ecommerce/modules/cart/integration/CartViewRepositoryTest.kt
+++ ecomerce-cart/src/test/kotlin/com/kotato/context/ecommerce/modules/cart/integration/CartViewRepositoryTest.kt
@@ -1,12 +1,11 @@
 package com.kotato.context.ecommerce.modules.cart.integration
 
+import com.kotato.assertSimilar.MatcherSimilar.assertSimilar
 import com.kotato.context.ecommerce.modules.cart.domain.view.CartViewRepository
 import com.kotato.context.ecommerce.modules.cart.stub.CartViewStub
 import com.kotato.shared.ContextStarterTest
 import org.junit.jupiter.api.Test
-import java.time.ZonedDateTime
 import javax.inject.Inject
-import kotlin.test.assertEquals
 
 open class CartViewRepositoryTest : ContextStarterTest() {
 
@@ -18,7 +17,7 @@ open class CartViewRepositoryTest : ContextStarterTest() {
 
         repository.save(view)
 
-        assertEquals(view, repository.search(view.id))
+        assertSimilar(view, repository.search(view.id))
     }
 
 }
