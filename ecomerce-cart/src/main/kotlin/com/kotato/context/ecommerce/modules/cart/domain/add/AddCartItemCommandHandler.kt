7af711a063ee66fd2867a3d7a829cc3e44109853
Ramon Aranda <ramon.aranda.r@gmail.com>
2018-01-21 18:41:10 +0100

    Upgrade cqrs lib version due to error on command/query handlers Change query/command handlers from axon to lib Add more tests

--- ecomerce-cart/src/main/kotlin/com/kotato/context/ecommerce/modules/cart/domain/add/AddCartItemCommandHandler.kt
+++ ecomerce-cart/src/main/kotlin/com/kotato/context/ecommerce/modules/cart/domain/add/AddCartItemCommandHandler.kt
@@ -3,8 +3,8 @@ package com.kotato.context.ecommerce.modules.cart.domain.add
 import com.kotato.context.ecommerce.modules.cart.domain.CartId
 import com.kotato.context.ecommerce.modules.cart.domain.CartItem
 import com.kotato.context.ecommerce.modules.item.domain.ItemId
+import com.kotato.cqrs.domain.command.CommandHandler
 import com.kotato.shared.money.Money
-import org.axonframework.commandhandling.CommandHandler
 import javax.inject.Named
 
 @Named
--- ecomerce-cart/src/test/kotlin/com/kotato/context/ecommerce/modules/cart/acceptance/SubtractCartItemControllerTest.kt
+++ ecomerce-cart/src/test/kotlin/com/kotato/context/ecommerce/modules/cart/acceptance/SubtractCartItemControllerTest.kt
@@ -1,9 +1,125 @@
 package com.kotato.context.ecommerce.modules.cart.acceptance
 
+import com.kotato.assertSimilar.MatcherSimilar.assertSimilar
+import com.kotato.context.ecommerce.modules.cart.adapter.create.CreateCartRestRequest
+import com.kotato.context.ecommerce.modules.cart.adapter.update.CartItemRestRequest
+import com.kotato.context.ecommerce.modules.cart.domain.Amount
+import com.kotato.context.ecommerce.modules.cart.domain.CartId
+import com.kotato.context.ecommerce.modules.cart.domain.CartItem
+import com.kotato.context.ecommerce.modules.cart.domain.view.CartViewRepository
+import com.kotato.context.ecommerce.modules.cart.stub.AddCartItemRestRequestStub
+import com.kotato.context.ecommerce.modules.cart.stub.CreateCartRestRequestStub
+import com.kotato.context.ecommerce.modules.cart.stub.SubtractCartItemRestRequestStub
+import com.kotato.context.ecommerce.modules.item.domain.ItemId
 import com.kotato.shared.ContextStarterTest
+import com.kotato.shared.TransactionalWrapper
+import com.kotato.shared.money.Money
+import io.restassured.RestAssured
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+import javax.inject.Inject
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class SubtractCartItemControllerTest : ContextStarterTest() {
 
+    @Inject private lateinit var wrapper: TransactionalWrapper
+    @Inject private lateinit var repository: CartViewRepository
 
+    @Test
+    fun `it should subtract item to cart`() {
+        val cartId = CreateCartRestRequestStub.random().also { createCart(it) }.id!!
+        val addItem = AddCartItemRestRequestStub.random(cartId = cartId)
+        val subItem = SubtractCartItemRestRequestStub.random(cartId = cartId,
+                                                             itemId = addItem.itemId!!,
+                                                             price = addItem.price!!,
+                                                             currency = addItem.currency!!,
+                                                             quantity = (addItem.quantity!! - 1) * -1)
+        patch(addItem)
+                .statusCode(HttpStatus.NO_CONTENT.value())
+        patch(subItem)
+                .statusCode(HttpStatus.NO_CONTENT.value())
+        wrapper.wrap { CartId.fromString(cartId.toString()).let(repository::search) }
+                .let {
+                    assertNotNull(it)
+                    assertTrue { it!!.cartItems.size == 1 }
+                    assertSimilar(it!!.cartItems.entries.first().toPair(),
+                                  CartItem(ItemId.fromString(addItem.itemId!!.toString()),
+                                           Money.of(addItem.price!!, addItem.currency!!)) to Amount(1))
+                }
+    }
+
+    @Test
+    fun `it should subtract item to cart with same quantity as actual has`() {
+        val cartId = CreateCartRestRequestStub.random().also { createCart(it) }.id!!
+        val addItem = AddCartItemRestRequestStub.random(cartId = cartId)
+        val subItem = SubtractCartItemRestRequestStub.random(cartId = cartId,
+                                                             itemId = addItem.itemId!!,
+                                                             price = addItem.price!!,
+                                                             currency = addItem.currency!!,
+                                                             quantity = (addItem.quantity!!) * -1)
+        patch(addItem)
+                .statusCode(HttpStatus.NO_CONTENT.value())
+        patch(subItem)
+                .statusCode(HttpStatus.NO_CONTENT.value())
+        wrapper.wrap { CartId.fromString(cartId.toString()).let(repository::search) }
+                .let {
+                    assertNotNull(it)
+                    assertTrue { it!!.cartItems.isEmpty() }
+                }
+    }
+
+    @Test
+    fun `it should subtract item to cart with more quantity than actual has`() {
+        val cartId = CreateCartRestRequestStub.random().also { createCart(it) }.id!!
+        val addItem = AddCartItemRestRequestStub.random(cartId = cartId)
+        val subItem = SubtractCartItemRestRequestStub.random(cartId = cartId,
+                                                             itemId = addItem.itemId!!,
+                                                             price = addItem.price!!,
+                                                             currency = addItem.currency!!,
+                                                             quantity = (addItem.quantity!!) * -1)
+        patch(addItem)
+                .statusCode(HttpStatus.NO_CONTENT.value())
+        patch(subItem)
+                .statusCode(HttpStatus.NO_CONTENT.value())
+        wrapper.wrap { CartId.fromString(cartId.toString()).let(repository::search) }
+                .let {
+                    assertNotNull(it)
+                    assertTrue { it!!.cartItems.isEmpty() }
+                }
+    }
+
+    @Test
+    fun `it should throw exception when trying to subtract a cart item because cart does not exists`() {
+        patch(SubtractCartItemRestRequestStub.random())
+                .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+    }
+
+    @Test
+    fun `it should throw exception when trying to subtract a cart item because no item in cart`() {
+        CreateCartRestRequestStub.random()
+                .also { createCart(it) }
+                .let {
+                    patch(SubtractCartItemRestRequestStub.random(cartId = it.id!!))
+                            .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                }
+    }
+
+    private fun patch(restRequest: CartItemRestRequest) = RestAssured.given()
+            .header("Content-Type", "application/json")
+            .body(objectMapper.writeValueAsString(restRequest))
+            .`when`()
+            .patch("/ecommerce/cart/main")
+            .then()
+
+    private fun createCart(restRequest: CreateCartRestRequest) {
+        RestAssured.given()
+                .header("Content-Type", "application/json")
+                .body(objectMapper.writeValueAsString(restRequest))
+                .`when`()
+                .post("/ecommerce/cart")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+    }
 
 }
