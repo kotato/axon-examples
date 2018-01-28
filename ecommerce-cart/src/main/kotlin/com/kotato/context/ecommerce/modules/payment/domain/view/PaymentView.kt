b49eaea359bddb8d5b20346928e41f5efd330eb8
Ramon Aranda <ramon.aranda.r@gmail.com>
2018-01-28 11:46:38 +0100

    Fix annotation at view

--- ecommerce-cart/src/main/kotlin/com/kotato/context/ecommerce/modules/payment/domain/view/PaymentView.kt
+++ ecommerce-cart/src/main/kotlin/com/kotato/context/ecommerce/modules/payment/domain/view/PaymentView.kt
@@ -9,10 +9,12 @@ import java.time.ZonedDateTime
 import javax.persistence.Embedded
 import javax.persistence.EmbeddedId
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 
 @Entity
 @DynamicUpdate
 data class PaymentView(@EmbeddedId val id: PaymentId,
                        val createdOn: ZonedDateTime,
                        @Embedded val price: Money,
-                       @Embedded val status: PaymentStatus) : Serializable
+                       @Enumerated(EnumType.STRING) val status: PaymentStatus) : Serializable
